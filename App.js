8fd7717e8d78d9c12174a02d6ac598c4fade3120
Maw Paing Thu <paingmawthu@gmail.com>
2022-08-31 23:05:56 +0900

    Flatlist

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import CardScreen from './.vscode/app/screens/CardScreen';
 import EncapsulatingStyleScreen from './.vscode/app/screens/EncapsulatingStyleScreen';
 import IconScreen from './.vscode/app/screens/IconScreen';
 import ListingDetailsScreen from './.vscode/app/screens/ListingDetailsScreen';
+import MessagesScreen from './.vscode/app/screens/MessagesScreen';
 import PaddingAndMarginScreen from './.vscode/app/screens/PaddingAndMarginScreen';
 import ShadowScreen from './.vscode/app/screens/ShadowScreen';
 import StylingTextScreen from './.vscode/app/screens/StylingTextScreen';
@@ -15,7 +16,7 @@ import WelcomeScreen from './.vscode/app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-      <ViewImageSreen/>
+      <MessagesScreen/>
   );
 }
 
